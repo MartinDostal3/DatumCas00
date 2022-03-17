799a48f80406edbbe8784b204df1284f33e2be77
dostalm.04 <dostalm.04@A321-14>
2022-03-17 12:48:11 +0100

    Datum00

---
+++ Datum01/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datum01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            string s = textBox1.Text;
+            char[] separators = { ' ', ';' };
+            string[] stringsplit = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            DateTime narozen = DateTime.Parse(textBox1.Text);
+            DateTime dnes = DateTime.Today;
+
+
+        }
+    }
+}
